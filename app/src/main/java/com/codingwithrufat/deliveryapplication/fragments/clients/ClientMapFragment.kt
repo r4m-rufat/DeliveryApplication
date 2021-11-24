3bc1987b6e0d3ed3a1f956b23377245a7fe76312
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-11-25 02:25:30 +0400

    added map markers

--- app/src/main/java/com/codingwithrufat/deliveryapplication/fragments/clients/ClientMapFragment.kt
+++ app/src/main/java/com/codingwithrufat/deliveryapplication/fragments/clients/ClientMapFragment.kt
@@ -1,11 +1,21 @@
 package com.codingwithrufat.deliveryapplication.fragments.clients
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.codingwithrufat.deliveryapplication.R
+import com.codingwithrufat.deliveryapplication.utils.constants.MAP_REQUEST_CODE
+import com.google.android.gms.gcm.Task
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -22,6 +32,8 @@ import com.google.android.gms.maps.model.MarkerOptions
  */
 class ClientMapFragment : Fragment(), OnMapReadyCallback{
 
+    private lateinit var currentLocation: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -29,6 +41,7 @@ class ClientMapFragment : Fragment(), OnMapReadyCallback{
         }
     }
 
+    @SuppressLint("MissingPermission")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,20 +49,59 @@ class ClientMapFragment : Fragment(), OnMapReadyCallback{
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_client_map, container, false)
 
+        val fusedLocationProvider = LocationServices.getFusedLocationProviderClient(requireContext())
         val mapFragment = childFragmentManager.findFragmentById(R.id.client_map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        if (isLocationPermissionGranted()){
+            val locationTask = fusedLocationProvider.lastLocation
+            locationTask.addOnSuccessListener { location ->
+                if (location != null){
+
+                    currentLocation = location
+                    mapFragment.getMapAsync(this)
+
+                }
+            }
+        }
 
         return view
     }
 
     override fun onMapReady(map: GoogleMap) {
-        val sydney = LatLng(-34.0, 151.10)
+        val baku = LatLng(40.4, 49.8)
         map.addMarker(
             MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
+                .position(baku)
+                .title("Courier")
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_motorcycle))
         )
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 6f))
+        map.addMarker(
+            MarkerOptions()
+                .position(LatLng(currentLocation.latitude, currentLocation.longitude))
+                .title("Client")
+        )
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLocation.latitude, currentLocation.longitude), 12f))
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                MAP_REQUEST_CODE
+            )
+            false
+        } else {
+            true
+        }
     }
 }
--- app/src/main/java/com/codingwithrufat/deliveryapplication/utils/constants/Constants.kt
+++ app/src/main/java/com/codingwithrufat/deliveryapplication/utils/constants/Constants.kt
@@ -3,3 +3,4 @@ package com.codingwithrufat.deliveryapplication.utils.constants
 const val TAG = "AppDebug"
 const val ONE_MINUTE_TIME = 60
 const val SECOND_MS = 1000L
+const val MAP_REQUEST_CODE = 101
